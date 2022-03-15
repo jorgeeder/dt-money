678c56bf4fe945ad6d4c1c57c0217655047fc041
Jorge Eder <jorgeeder.js@gmail.com>
2022-03-15 05:27:04 -0300

    Context API in React

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import Modal from 'react-modal';
 import { Dashboard } from './components/Dashboard';
 import { Header } from './components/Header';
 import { NewTransactionModal } from './components/NewTransactionModal';
+import { TransactionsContext } from './TransactionsContext';
 
 import { GlobalStyle } from './styles/global';
 
@@ -19,7 +20,7 @@ export function App() {
   }
   
   return (
-    <>
+    <TransactionsContext.Provider value={[]}> 
       <Header onOpenNewtransactionModal={handleOpenNewTransactionModal} />
 
       <Dashboard />
@@ -28,6 +29,6 @@ export function App() {
         onRequestClose={handleCloseNewTransactionModal}
       />
       <GlobalStyle />
-    </>
-  )
+    </TransactionsContext.Provider>
+  );
 }
