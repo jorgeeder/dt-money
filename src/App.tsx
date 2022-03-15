e74ed9210ee1a2dc85051db4d1cd2964dec64230
Jorge Eder <jorgeeder.js@gmail.com>
2022-03-15 07:44:06 -0300

    loading transactions

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import Modal from 'react-modal';
 import { Dashboard } from './components/Dashboard';
 import { Header } from './components/Header';
 import { NewTransactionModal } from './components/NewTransactionModal';
-import { TransactionsContext } from './TransactionsContext';
+import { TransactionsProvider } from './TransactionsContext';
 
 import { GlobalStyle } from './styles/global';
 
@@ -20,7 +20,7 @@ export function App() {
   }
   
   return (
-    <TransactionsContext.Provider value={[]}> 
+    <TransactionsProvider> 
       <Header onOpenNewtransactionModal={handleOpenNewTransactionModal} />
 
       <Dashboard />
@@ -29,6 +29,6 @@ export function App() {
         onRequestClose={handleCloseNewTransactionModal}
       />
       <GlobalStyle />
-    </TransactionsContext.Provider>
+    </TransactionsProvider>
   );
 }
